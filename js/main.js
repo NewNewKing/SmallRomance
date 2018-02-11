702a337a0f7fe9fad82bbb0b62d4a764ffa51b2b
leslie <evil_outman@163.com>
2018-02-11 19:35:32 +0800

    fix

--- js/main.js
+++ js/main.js
@@ -126,6 +126,8 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 var width = 360;
 var height = 600;
 
+//与时间有关的设置均为毫秒数，本文件底部会自动转化为帧数。
+
 var config = function () {
 
 	return {
@@ -133,7 +135,7 @@ var config = function () {
 		width: width, //---不建议改动
 		height: height, //---不建议改动
 		//canvas
-		canvases: ['fall', 'bg', 'firework', 'dialogue'],
+		canvases: ['fall', 'bg', 'firework', 'dialogue'], //---不建议改动
 		// 飘落微粒产生间隔
 		snowInterval: 60,
 		heartInterval: 15,
@@ -168,7 +170,7 @@ var config = function () {
 			color3: 'red',
 			color4: '#ffff00',
 			color5: '#00ff00',
-			color6: '#0000ff',
+			color6: '#00ffff',
 			color7: '#fff'
 		},
 		dialogue: [{ type: 1, name: '马飞飞', txt: '同志们，正月十五晚上，我们怎么玩？' }, { type: 2, name: '软软', txt: '想怎么玩！就怎么玩！安排巴适！' }, { type: 3, name: '安安', txt: '真的？带我去喝茶！我要喝10个！' }, { type: 4, name: '轩轩', txt: '喝啥子茶哦，大家都拖家带口的。' }, { type: 5, name: '小勇、灿灿', txt: '就是，都带了家属的，还是炸金花算了。' }, { type: 6, name: '李湘江', txt: '还是放火炮嘛，好耍！' }, { type: 7, name: '王兴欣', txt: '就是，放火炮！走起！' }],
@@ -202,21 +204,23 @@ var config = function () {
 		//阶段四
 		titleWords: '新年快乐|万事如意|心想事成',
 		titleOpt: {
+			gap: 3,
+			size: 70, //最后字的大小
 			delay: 4000, //
 			distance: 120, //行间距
 			e: 5000 //速率
 		},
 
+		/*******均不建议改动********/
 		//字的参数
-		shape: { //---不建议改动
+		shape: {
 			mini: 1, //组成字的粒子数  mini越大 粒子数越少
 			gap: 4 //粒子的间隔数 必须能被width整除
 		},
-		word: { //---不建议改动
+		word: {
 			size: 70,
 			y: 120
-		} //---不建议改动
-
+		}
 
 	};
 }();
@@ -986,6 +990,8 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 				this.titleOpt.start = true;
 				this.titleOpt.time = _global2.default.titleOpt.e + _global2.default.titleOpt.delay;
 
+				_global2.default.shape.gap = _global2.default.titleOpt.gap;
+				_global2.default.word.size = _global2.default.titleOpt.size;
 				_global2.default.word.y += _global2.default.titleOpt.distance;
 
 				var dots = this.getDotsPostion(this.titleWords.shift());
@@ -995,7 +1001,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 						y: dot.y,
 						xStart: _util2.default.random(0, _global2.default.width),
 						yStart: _util2.default.random(-100, 0),
-						size: 6,
+						size: 5,
 						e: _global2.default.titleOpt.e
 					};
 					_this4.titleDots.push(new _TitleParticle2.default(option));
