d3655327fd60815edc2e6e11205ba798de8b347d
王兴欣 <evil_outman@163.com>
2022-01-16 14:58:23 +0800

    修改配置文件

--- src/config/global.js
+++ src/config/global.js
@@ -42,7 +42,7 @@ const config = (function(){
 		fallType: 'snow',
 
 		// 从阶段几开始 1、对话 2、天黑 3、烟花 4、展示文字
-		step: 1,
+		step: 3,
 		// 阶段一
 		dialogueOpt:{ 
 			interval: 1500,  //两句话的间隔时间
@@ -57,6 +57,8 @@ const config = (function(){
 			color7: '#fff',
 		},
 		// type对应上面的color与font  若没有对应的 则默认为color1或font1
+    // type: 7 就是使用 color7与font7 因为没有font7 所以则会使用font1
+    // 最后的样式就是color7与font1
 		dialogue:[
 			{type:7, name:'小孩A', txt:'程二狗，快出来放炮了'},
 			{type:7, name:'小孩A', txt:'我今天买了很多擦炮'},
@@ -73,48 +75,70 @@ const config = (function(){
 		// 阶段二
 		sunset: 6000,   // 天黑时间
 
-	    // 阶段三
-		fireworkInterval:[1000, 4000],// 烟花产生间隔 //---不建议改动
-		//烟花的属性
-		fireworks:{ 
-			x: undefined,
-			y: height,
-			xEnd: undefined,
-			yEnd: undefined,
-			size: 2,
-			radius: [1, 2],  //烟花半径
-			velocity: 3,  //速率
-			opacity: 0.8,
-			count: [150,250],   //炸裂后粒子数
-			wait: undefined,  //消失后 => 炸裂  等待时间
-			color: undefined,  //烟花颜色
-		},
-		fireWords:'祝大家|新的一年|生意兴隆|财源滚滚|万事如意|步步高升|福寿安康|笑口常开|最重要的是|没有BUG',  // '|' 为分隔符
-		// hue:210 lightness 0
-		skyColor:'hsla({hue}, 60%, {lightness}%, 0.2)',	
-		fireOpt: {
-			wordInterval: 2000, //每段话出现的间隔时间
-		},
+    // 阶段三
+    fireworkInterval:[1000, 4000],// 烟花产生间隔 每隔1-4s就会随机产生一个烟花
+    //烟花的属性
+    fireworks:{ 
+      // x、y是出生时坐标 不用改动
+      x: undefined, 
+      y: height,
+      // end是爆炸地点的坐标 不用改动
+      xEnd: undefined,
+      yEnd: undefined,
+      // size是烟花升空时看到的小点点的大小 不用改动
+      size: 2,
+      // 烟花升空的速度
+      velocity: 3,
+      // 烟花的透明度
+      opacity: 0.8,
+      // 烟花半径大小的范围  最终的结果需要 半径
+      radius: [1, 2],
+      // 炸裂后粒子数范围 粒子数越多烟花越好看 但是可能会造成卡顿 
+      count: [150,250],
+      //消失后 => 炸裂  等待时间
+      wait: undefined, 
+      //烟花颜色 未指定颜色则是随机的颜色
+      color: undefined,  
+    },
+    // '|' 为分隔符 
+    fireWords:'没有BUG', 
+    fireOpt: {
+      //每段话出现的间隔时间
+      wordInterval: 2000, 
+    },
     //烟花字的参数
     word:{  
-			gap: 3,   //粒子的间隔数 gap越大 粒子数越少
+      // 粒子的间隔数 gap越大 粒子数越少
+      gap: 3,   
+      // 文字的大小
       size: 70,
+      // 展示的位置 0表示最顶部
       y: height / 4
     }, 
+		// hue:210 lightness 0
+		skyColor:'hsla({hue}, 60%, {lightness}%, 0.2)',	
 
-	
 		//阶段四
-		titleWords: '天天开心|最为重要|祝大家|虎年大吉', // '|' 为分隔符
-		titleOpt:{
-			y: 20,
-			gap: 3,
-			size: 60,  //最后字的大小
-			pSize: 6,
-			//color: 'rgb(180,4,4)',// 可以缺省
-			delay: 3000, //
-			distance: 100, //行间距
-			e: 2000 //速率
-		},
+		titleWords: '天天开心|最为重要|祝大家|虎年大吉', // '|' 为分隔符
+    titleOpt:{
+      // 第一排的字距离顶部的距离
+      y: 20,
+      // 粒子的间隔数 gap越大 粒子数越少
+      gap: 3,
+      // 字的大小
+      size: 60,
+      // 小爱心的大小
+      pSize: 6,
+      // 小爱心的颜色 不指定的话就都是随机的
+      //color: 'rgb(180,4,4)',// 可以缺省
+      // 每段文字出现的间隔时间
+      delay: 3000,
+      // 每段文字的间距
+      distance: 100, //行间距
+      // 小爱心出现 => 指定位置的时间 
+      // 数字越小形成文字越快
+      e: 2000
+    },
 	}
 })();
 
